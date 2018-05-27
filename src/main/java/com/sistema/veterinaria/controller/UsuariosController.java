9973b984e384db741f6aca7f563a320fcfd24f9a
Oliver <oliversoria@hotmail.com>
2018-05-27 18:18:54 -0500

    Commit de prueba

--- src/main/java/com/sistema/veterinaria/controller/UsuariosController.java
+++ src/main/java/com/sistema/veterinaria/controller/UsuariosController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/administrador")
 public class UsuariosController {
-	
+
 	@RequestMapping(value = "/usuarios", method = RequestMethod.GET)
 	public String home(Model model) {
 		return "/usuarios/usuarios";
